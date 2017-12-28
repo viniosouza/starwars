71fb74a0ed841d9c55359a04ca51ac63fb1a1ae7
viniosouza <vinicios2508@gmail.com>
2017-12-28 04:44:06 -0200

    Modal started

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,11 +1,75 @@
 import React, { Component } from "react";
 import "./styles/CardList.scss";
+import { PlusButton } from "../Button";
+import { Link } from "react-router-dom";
+
 class CardList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modalOpened: false
+    };
+
+    this.modalToggle = this.modalToggle.bind(this);
+  }
+
+  modalToggle() {
+    this.setState({ modalOpened: !this.state.modalOpened });
+  }
+
   render() {
+    const coverClass = this.state.modalOpened
+      ? "modal-cover modal-cover-active"
+      : "modal-cover";
+    const containerClass = this.state.modalOpened
+      ? "modal-container modal-container-active"
+      : "modal-container";
     return (
       <div className="Card">
+        {/* Modal */}
+        <div className={containerClass}>
+          <div className="modal-header">
+            <img
+              className="modal-cover-img"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2000px-Star_Wars_Logo.svg.png"
+              alt="starwars"
+            />
+          </div>
+          <div className="modal-body">
+            <p>{this.props.name}</p>
+            <p>
+              {this.props.population
+                ? this.props.population
+                : this.props.height}
+            </p>
+            <p>
+              {this.props.diameter ? this.props.diameter : this.props.gender}
+            </p>
+            <p>
+              {this.props.climate ? this.props.climate : this.props.birth_year}
+            </p>
+            <p>
+              {this.props.gravity ? this.props.gravity : this.props.hair_color}
+            </p>
+            <p>{this.props.films ? this.props.films : this.props.skin_color}</p>
+
+            <Link to="#">
+              <p>{this.props.homeworld ? this.props.homeworld : null}</p>
+            </Link>
+          </div>
+          <div className="modal-footer" />
+        </div>
+
+        <div className={coverClass} onClick={this.modalToggle} />
+        {/* PlussButton */}
+        <div className="Card--hoverOverlay">
+          <button className="plusButton" onClick={this.modalToggle}>
+            <PlusButton />
+          </button>
+        </div>
         <img
-          className="Card-img"
+          className="Card-star"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2000px-Star_Wars_Logo.svg.png"
           alt="starwars"
         />
